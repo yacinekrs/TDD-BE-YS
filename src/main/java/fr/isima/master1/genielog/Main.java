6f5a1eff08d1df29f617e76427b2f0e036a1d783
mykhorsi <mykhorsi@alvin-vm-prd.local>
2022-11-16 15:53:00 +0100

    first commit

---
+++ src/main/java/fr/isima/master1/genielog/Main.java
@@ -0,0 +1,5 @@
+package fr.isima.master1.genielog;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/fr/isima/master1/genielog/RoleTest.java
@@ -0,0 +1,6 @@
+package fr.isima.master1.genielog;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RoleTest {
+  
+}
